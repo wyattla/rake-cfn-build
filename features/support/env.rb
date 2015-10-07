5a4fbd24dc44a4d7955a89d24ff66c50fd639013
Ali Asad Lotia <ali.asad.lotia@gmail.com>
2015-10-07 16:31:02 +0100

    Streamline setup process.
    - Add README file.
    - Remove potentially dangerous `envvars_test` file which may result in
      users checking in AWS creds and add above file to .gitignore
    - Improve env var check to ensure each required env var is defined and
      exit if not defined or blank.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,42 +1,11 @@
 require 'aruba/cucumber'
 
-
-def help
-
-  message = <<EOF
-
-Error: 
-
-One or more of the following environment variables is not
-defined. To test you can use the rake embedded project or
-a custom one. To do so just run the lines above in the shell
-and complete with the account credentials
-
-  # Test project specific variables
-  export EV_BUCKET_NAME=els-evise-nonprod-rake-cfn-build
-  export EV_PROJECT_NAME=testproj
-  export EV_ENVIRONMENT=test
-  export EV_GIT_PATH=resources/test_app
-  export EV_CREATE_IF_NOT_EXIST=true
-  export EV_CFN_STACK_NAME=test-testproj-testapp
- 
-  # AWS credentials
-  export AWS_ACCESS_KEY_ID=
-  export AWS_SECRET_ACCESS_KEY=
-  export AWS_DEFAULT_REGION=
-
-EOF
-
-  puts message
-
-end
-
 # Show help message if environment variables are not defined
 %w(AWS_ACCESS_KEY_ID AWS_DEFAULT_REGION AWS_SECRET_ACCESS_KEY
  EV_BUCKET_NAME EV_CFN_STACK_NAME EV_CREATE_IF_NOT_EXIST 
 EV_ENVIRONMENT EV_GIT_PATH EV_PROJECT_NAME).each do |var|
-  unless ENV.include? var
-    help
+  unless (ENV.include? var) && (ENV[var] != "")
+    puts "Required environment variable #{var} is empty or not defined."
     exit 1
   end
 end
